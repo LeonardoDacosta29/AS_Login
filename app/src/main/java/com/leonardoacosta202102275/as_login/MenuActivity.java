bf2f8e7a8d883a7744a607825ef44d06b9d04fe9
LeonardoDacosta29 <dacostaleonardo137@gmail.com>
2023-07-09 12:32:06 +0700

    Login bisa
    berhasil

---
+++ app/src/main/java/com/leonardoacosta202102275/as_login/MenuActivity.java
@@ -0,0 +1,14 @@
+package com.leonardoacosta202102275.as_login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+    }
+}
